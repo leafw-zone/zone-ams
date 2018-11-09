d7e66920e399c1cdb14a3cb53d9dcfed2df7caf5
CareyWYR <wyr95626@163.com>
2018-11-10 00:37:28 +0800

    'db'

--- doc/db.sql
+++ doc/db.sql
@@ -9,7 +9,7 @@ CREATE TABLE `account_log` (
   `update_time` datetime DEFAULT NULL,
   `update_by` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`account_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `account_main` (
   `account_id` varchar(20) NOT NULL COMMENT '账户id',
@@ -22,4 +22,16 @@ CREATE TABLE `account_main` (
   `update_time` datetime DEFAULT NULL,
   `update_by` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`account_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `consume_config` (
+  `consume_id` varchar(11)  NOT NULL,
+  `consume_name` varchar(16)  DEFAULT NULL,
+  `change_type` varchar(2)  DEFAULT NULL,
+  `is_deleted` varchar(2)  DEFAULT NULL,
+  `create_by` varchar(16)  DEFAULT NULL,
+  `update_by` varchar(16)  DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`consume_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
